a090e445a8af2ce6ee9063e39dff0d4cadabf5d9
hyeongeunHwang <hhg7899@gmail.com>
2022-11-01 12:08:28 +0900

    Revert "대문 페이지와 자기소개 페이지 만들기"
    This reverts commit 228543c6364c74fc4ac51b6ee384eccce0970188.

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path("admin/", admin.site.urls),
-    path('', include('single_pages.urls')),
 ]
