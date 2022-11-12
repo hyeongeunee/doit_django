bf5c7670b24da2d85f92cc1162e5db4822c039d7
hyeongeunHwangcom <hhg7899@gmail.com>
2022-11-12 20:43:57 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -39,3 +39,7 @@ class Post(models.Model):
     
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+    
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    
--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
-    ordering = '-pk'
+    ordering = '-pk'    
     
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -24,7 +24,24 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
             # template_name = 'blog/index.html'
-
+    def category_page(request, slug):
+        if slug == 'no_category':
+            category = '미분류'
+            post_list = Post.objects.filter(category=None)
+        else:
+            category = Category.objects.get(slug=slug)
+            post_list = Post.objects.filter(category=category)
+                    
+        return render(
+            request,
+            'blog/post_list.html',
+            {
+                'post_list': post_list,
+                'categories': Category.objects.all(),
+                'no_category_post+count' : Post.objects.filter(category=None).count(),
+                'category':category,
+            }
+        )
 # def single_post_page(request, pk):
 #     post = Post.objects.get(pk=pk)
 #     return render(
@@ -37,3 +54,9 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
